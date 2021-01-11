10c099212ff4e36a26e88f2f1a2ae18ba0093306
overflowerror <mail@overflowerror.com>
2021-01-12 00:36:48 +0100

    removed awk from shell version

--- shell/fibonacci.sh
+++ shell/fibonacci.sh
@@ -7,7 +7,7 @@ fibonacci() {
 		return 0
 	fi
 
-	(tee /dev/stderr | tail -n 2 | awk '{s += $1} END {print s}') 2>&1 | fibonacci $((i - 1))
+	(tee /dev/stderr | tail -n 2 | paste -s -d "+" | bc) 2>&1 | fibonacci $((i - 1))
 }
 
 fibonacci 5 <<EOF
